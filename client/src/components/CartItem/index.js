5483b79d4a470df891ffc0c2a26a87aa9eff82da
mrsjlwhite <jlwhite.codes@gmail.com>
2022-05-22 13:52:00 -0700

    💄 Created Shopping Cart layout, added components

---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const CartItem = ({ item }) => {
+  return (
+    <div className="flex-row">
+      <div>
+        <img
+          src={`/images/${item.image}`}
+          alt=""
+        />
+      </div>
+      <div>
+        <div>{item.name}, ${item.price}</div>
+        <div>
+          <span>Qty:</span>
+          {/* temp readOnly till onChange is added */}
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            readOnly
+          />
+          <span
+            role="img"
+            aria-label="trash"
+          >
+            🗑️
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;

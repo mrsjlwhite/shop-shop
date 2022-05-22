e02e0d521661bb2e241ad908baaba7d6e98fbea5
mrsjlwhite <jlwhite.codes@gmail.com>
2022-05-22 15:14:38 -0700

    🚸 Able to retrieve product storage without internet

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -17,17 +17,25 @@ function ProductList() {
   const { products } = state;
   
   useEffect(() => {
-    // if there's data to be stored
+    // because we're never in a state of loading data
     if (data) {
-      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
   
-      // but let's also take each product and save it to IndexedDB using the helper function 
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
       });
     }
   }, [data, loading, dispatch]);
